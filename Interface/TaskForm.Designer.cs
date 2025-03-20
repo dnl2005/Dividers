bfa8333fd196ca4f62684c4bcf77af44a05c69cb
User <User@WIN-J7TNE5BCSG4>
2025-03-20 12:26:14 +0300

    Taskforma

--- Interface/TaskForm.Designer.cs
+++ Interface/TaskForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Interface
 {
-    public partial class TaskForm
+    partial class TaskForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,27 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            fileSystemWatcher1 = new FileSystemWatcher();
             label1 = new Label();
             label2 = new Label();
-            textBox1 = new TextBox();
             label3 = new Label();
             button1 = new Button();
-            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            textBox1 = new TextBox();
             SuspendLayout();
-            // 
-            // fileSystemWatcher1
-            // 
-            fileSystemWatcher1.EnableRaisingEvents = true;
-            fileSystemWatcher1.SynchronizingObject = this;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Monotype Corsiva", 24F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            label1.Location = new Point(200, 45);
+            label1.Location = new Point(353, 48);
             label1.Name = "label1";
-            label1.Size = new Size(660, 49);
+            label1.Size = new Size(458, 49);
             label1.TabIndex = 0;
             label1.Text = "Наибольший общий делитель";
             label1.Click += this.label1_Click;
@@ -56,67 +49,65 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Monotype Corsiva", 16.2F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            label2.Location = new Point(340, 118);
+            label2.Font = new Font("Monotype Corsiva", 13.8F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            label2.Location = new Point(420, 128);
             label2.Name = "label2";
-            label2.Size = new Size(341, 34);
+            label2.Size = new Size(282, 27);
             label2.TabIndex = 1;
             label2.Text = "Введите число для нахождения";
             label2.Click += this.label2_Click;
             // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(397, 189);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(219, 27);
-            textBox1.TabIndex = 2;
-            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Monotype Corsiva", 16.2F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            label3.Location = new Point(428, 293);
+            label3.Font = new Font("Monotype Corsiva", 13.8F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            label3.Location = new Point(517, 300);
             label3.Name = "label3";
-            label3.Size = new Size(130, 34);
-            label3.TabIndex = 3;
+            label3.Size = new Size(108, 27);
+            label3.TabIndex = 2;
             label3.Text = "Результат";
             label3.Click += this.label3_Click;
             // 
             // button1
             // 
-            button1.Font = new Font("Monotype Corsiva", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            button1.Location = new Point(428, 242);
+            button1.Font = new Font("Monotype Corsiva", 12F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            button1.Location = new Point(517, 234);
             button1.Name = "button1";
-            button1.Size = new Size(152, 29);
-            button1.TabIndex = 4;
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
             button1.Text = "Найти";
             button1.UseVisualStyleBackColor = true;
             // 
-            // Form2
+            // textBox1
+            // 
+            textBox1.Location = new Point(439, 184);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(263, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // TaskForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(199, 210, 242);
-            ClientSize = new Size(1045, 576);
+            ClientSize = new Size(1111, 591);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Controls.Add(label3);
-            Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "TaskForm";
-            Text = "Наибольший Общий Делитель";
-            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            Text = "Form2";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private FileSystemWatcher fileSystemWatcher1;
         private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox textBox1;
         private Button button1;
+        private TextBox textBox1;
     }
 }
